6228573a4bfbb0b5222230248bf2e1ae73ed1a3a
JUCILY117 <111986929+JUCILY117@users.noreply.github.com>
2025-08-05 00:01:53 +0530

    fixed a UI bug

--- src/components/common/MobileWarning.jsx
+++ src/components/common/MobileWarning.jsx
@@ -20,7 +20,7 @@ export default function MobileWarning() {
     <div className="fixed inset-0 bg-gradient-to-br from-[#0e1c1b] via-[#111e25] to-[#14232e] flex flex-col justify-center items-center p-6 text-center z-50">
         <ProfilePicture size={256} />
       <p className="text-teal-400 font-semibold text-lg md:text-xl leading-relaxed max-w-md drop-shadow-lg mt-10">
-        Sorry, you&apos;re visiting my website on <span className="uppercase font-extrabold text-teal-300">mobile or tablet</span> devices, yet—cause my lazy ass hasn&apos;t made it responsive.
+        Sorry, you can&apos;t visit my website on <span className="uppercase font-extrabold text-teal-300">mobile or tablet</span> devices yet, cause my lazy ass hasn&apos;t made it responsive.
       </p>
       <p className="mt-6 font-mono text-sm text-teal-200 tracking-wide">
         (But hey, feel free to check it out on desktop, hehehehehe)
